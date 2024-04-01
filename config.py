8d728e6fe313599d0b8861b21ff2350b68421bc0
ArabRobo <121577164+ArabRobo@users.noreply.github.com>
2024-04-01 23:19:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6620719173:AAHc_EGCKSlprJi7GUejIUbow2Ipr-P72J8")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6545067154:AAEyO3uPns0FeQyPGW0-aRRDeujsLDKlxqs")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "10064016"))
@@ -20,7 +20,7 @@ APP_ID = int(os.environ.get("APP_ID", "10064016"))
 API_HASH = os.environ.get("API_HASH", "b9ca5d9a6c625a890af28db4adf50cf4")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002127081921"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001824289657"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "Arabnihnge")
@@ -39,8 +39,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://zzmtfmli:qGDEmy5P3Ux_zpvsMsQDs-lF7Uq6fggI@floppy.db.elephantsql.com/zzmtfmli")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001687754020"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1002016224705"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001604781781"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "-1001929470695"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
